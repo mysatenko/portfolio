8c74b16ce6fac006f38a85835910413aa039f9c3
EvhenM <mysatenko1@gmail.com>
2020-07-10 01:07:42 +0300

    portfolio

---
+++ app.js
@@ -0,0 +1,13 @@
+$(function() {
+    $('[data-scroll]').on('click', function(event){
+        event.preventDefault();
+        blockId = $(this).data('scroll');
+        blockOffset = $(blockId).offset().top;
+
+        $('html, body').animate({
+            scrollTop: blockOffset - 50
+        }, 700);
+        
+    })
+    
+});
